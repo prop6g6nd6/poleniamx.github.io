10bd73048cb6908dbf48e25ef979a4ffa79f2d4a
Jorge Vazquez <45601370+prop6g6nd6@users.noreply.github.com>
2024-10-11 19:51:30 +0000

    Removed index.html and integrated content into Next.js app

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App!</h1>
+      <p>This is a simple Next.js application.</p>
+    </div>
+  );
+};
+
+export default Home;
